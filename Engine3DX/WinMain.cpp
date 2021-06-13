564188fd0458a617a870514cd4bdbc76c8bea1ab
v-p116 <v_p116@txstate.edu>
2021-06-13 11:23:35 -0500

    Tested some messages procedures

--- Engine3DX/WinMain.cpp
+++ Engine3DX/WinMain.cpp
@@ -1,5 +1,7 @@
-
 #include <Windows.h>
+#include <string>
+#include <sstream>
+
 
 // Def Win Proc
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -7,6 +9,31 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	case WM_CLOSE:
 		// Quit Code = 31
 		PostQuitMessage(31);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, "Respects");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, "Danger");
+		}
+		break;
+	case WM_CHAR:
+		{
+		static std::string title;
+		title.push_back((char)wParam);
+		SetWindowText(hWnd, title.c_str());
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		{
+		POINTS pt = MAKEPOINTS(lParam);
+		std::ostringstream oss;
+		oss << "(" << pt.x << ", " << pt.y << ")";
+		SetWindowText(hWnd, oss.str().c_str());
+		}
 		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -18,9 +45,9 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow
 )
-{
 
-	const auto pClassName = L"Engine3DX";
+{
+	const auto pClassName = "Engine3DX";
 	// Configure Win Class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
@@ -41,7 +68,7 @@ int CALLBACK WinMain(
 
 	// Create Win Instance
 	HWND hWnd = CreateWindowEx(
-		0, pClassName, L"Window",
+		0, pClassName, "Window",
 		WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
 		300, 300, 1280, 720,
 		nullptr, nullptr, hInstance, nullptr
