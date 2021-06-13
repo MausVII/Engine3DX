b0e1cc1a1f9d0117b043ba62e53c632e40f90577
v-p116 <v_p116@txstate.edu>
2021-06-12 20:40:23 -0500

    Window created, and it was good

--- Engine3DX/WinMain.cpp
+++ Engine3DX/WinMain.cpp
@@ -1,7 +1,42 @@
+
 #include <Windows.h>
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
-	int nCmdShow )
+	int nCmdShow
+)
+{
+
+	const auto pClassName = L"Engine3DX";
+	// Configure Win Class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	// Register Win Class
+	RegisterClassEx(&wc);
+
+	// Create Win Instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName, L"Window",
+		WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
+		300, 300, 1280, 720,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (true);
+	return 0;
+}
